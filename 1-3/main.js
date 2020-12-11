3214e3cc53fa7e3304a343aad5e8c2d1e2f1bfc0
yuya-yuya <yytyfp5115@icloud.com>
2020-12-11 21:13:46 +0900

    initial commit

---
+++ 1-3/main.js
@@ -0,0 +1,24 @@
+// 問1
+let a = 12;
+let b = 14;
+
+console.log(a !== 10); 
+console.log(10 <= b && b < 20 && b % 2 === 0);
+
+// 問2
+var x =10;
+
+if(10 <= x && x < 20){
+  console.log("成功です");
+}else{
+  console.log("失敗です");
+}
+
+// 問3
+var y =0;
+
+if (y % 2 === 0){
+  console.log("偶数です");
+}else{
+  console.log("奇数です");
+}
