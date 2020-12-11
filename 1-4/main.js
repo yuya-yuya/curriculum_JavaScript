769f281195330a285610b85a6a31bdc4444b2ea2
yuya-yuya <yytyfp5115@icloud.com>
2020-12-11 21:45:15 +0900

    initial commit

---
+++ 1-4/main.js
@@ -0,0 +1,11 @@
+for(x = 1;x < 101;x++){
+  if(x % 3 === 0 && x % 5 === 0){
+    console.log("FizzBuzz!! ");
+  }else if(x % 3 === 0 ){
+    console.log("Fizz!");
+  }else if(x % 5 === 0){
+    console.log("BUzz!");
+  }else{
+    console.log(x);
+  }
+}
